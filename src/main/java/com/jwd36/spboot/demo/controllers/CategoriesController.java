3d47b9a2559b4da70d7a23ce81228ba8d7c61217
DESKTOP-EGGHC29\yemya <xino3424@gmail.com>
2023-03-05 11:11:55 +0630

    aa

---
+++ src/main/java/com/jwd36/spboot/demo/controllers/CategoriesController.java
@@ -0,0 +1,16 @@
+package com.jwd36.spboot.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/category")
+public class CategoriesController {
+
+    @GetMapping(value = "/create")
+    public String createCategory(){
+return "create";
+    }
+
+}
